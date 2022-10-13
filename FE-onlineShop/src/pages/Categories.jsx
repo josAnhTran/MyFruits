cc03d70a80f922e5b8ba1700a8986cf19069d462
josAnhTran <jostranvananh@gmail.com>
2022-10-13 18:20:52 +0700

    update Categories FE

--- FE-onlineShop/src/pages/Categories.jsx
+++ FE-onlineShop/src/pages/Categories.jsx
@@ -1,21 +1,235 @@
-import React, { useEffect } from 'react'
-import {Button, Layout} from 'antd';
-import {Content} from 'antd/lib/layout/layout';
+import React, { useEffect, useState } from 'react'
+
 import axios from 'axios';
+
+import {Button, Layout, Table, Form, Input, Popconfirm, message, Space, notification, Modal } from 'antd';
+import {Content} from 'antd/lib/layout/layout';
+import {DeleteOutlined, EditOutlined} from '@ant-design/icons';
+
 import { URLCategory } from '../functions/constants';
 
+
+
 function Categories() {
+
+  const columns =[
+    {
+      title: 'Tên danh mục',
+      key: 'name',
+      dataIndex: 'name',
+      width: '15%',
+      render: (text) => {
+        const style = {
+          fontWeight: 600,
+        }
+        return <span style={style}>{text}</span>
+      }
+    },
+    {
+      title: 'Mô tả',
+      key: 'description',
+      dataIndex: 'description'
+    },
+    {
+      title: '',
+      key: 'actions',
+      width: '1%',
+      render: (record) => {
+        return (
+          <Space>
+            <Button
+            icon={<EditOutlined />}
+            type='primary' 
+            onClick= {() => {
+              setIsModalOpen(true)
+            }}
+            >
+            </Button>
+            <Popconfirm 
+              overlayInnerStyle={{width: 300}}
+              title='Bạn muốn xóa không ?' 
+              okText= 'Đồng ý'
+              cancelText= 'Đóng'
+              onConfirm={() =>{
+                const {_id} = record
+                  axios.delete(URLCategory+ '/delete-id/' + _id)
+                  .then(response => {
+                    if(response.status === 200) {
+                      setRefresh(e=> !e)
+                      message.info('Xóa thành công')
+                    }
+                  })
+              }}
+            >
+              <Button
+                icon={<DeleteOutlined />}
+                type='danger' style={{fontWeight: 600}}
+                onClick= {() => {}}
+              >
+              </Button>
+            </Popconfirm>
+          </Space>
+       
+        )
+      }
+    }
+  ]
+
+  const [categories, setCategories] = useState([])
+  const [refresh, setRefresh] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const [createForm] = Form.useForm()
+  const [updateForm] = Form.useForm()
+
+
+  const handleOk  = () =>{
+    updateForm.submit()
+    // setIsModalOpen(false)
+  }
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
   useEffect(()=>{
     axios.get(URLCategory)
     .then(response => {
-      console.log(response.data)
+     setCategories(response.data)
     })
-  },[])
+  },[refresh])
   return (
     <Layout>
-    <Content>
-    <Button type='primary'>OK</Button>
+    <Content style={{padding: 24}}>
+    <Form
+      form={createForm}
+      name="F"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        name: "",
+        description: ''
+      }}
+      onFinish={(values) => {
+        //POST
+        axios.post( URLCategory +'/insert', values)
+        .then(response => {
+          if(response.status === 201) {
+            setRefresh(e => !e)
+            createForm.resetFields()
+            notification.info({message: 'Thông báo', description: 'Thêm mới thành công'})
+          }
+        })
+        console.log(values)
+      }}
+      onFinishFailed={(error) => {
+        console.error(error)
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Tên danh mục"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: 'Vui lòng nhập tên danh mục!',
+          },
+        ]}
+        style={{fontWeight: 600}}
+        hasFeedback
+      >
+        <Input placeholder='Tên danh mục mới'/>
+      </Form.Item>
 
+      <Form.Item
+        label="Mô tả danh mục mới"
+        name="description"
+        style={{fontWeight: 600}}
+
+      >
+        <Input placeholder='Mô tả danh mục mới'/>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Tạo mới
+        </Button>
+      </Form.Item>
+    </Form>
+    <Table rowKey='_id' columns={columns} dataSource={categories} 
+          pagination ={false}
+    />
+
+    <Modal 
+      title="Chỉnh sửa thông tin danh mục" 
+      open={isModalOpen} 
+      onOk={handleOk} 
+      onCancel={handleCancel}
+    >
+    <Form
+      form={updateForm}
+      name="updateForm"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        name: "",
+        description: ''
+      }}
+      onFinish={(values) => {
+        // //POST
+        // axios.post( URLCategory +'/insert', values)
+        // .then(response => {
+        //   if(response.status === 201) {
+        //     setRefresh(e => !e)
+        //     updateForm.resetFields()
+        //     notification.info({message: 'Thông báo', description: 'Thêm mới thành công'})
+        //   }
+        // })
+        console.log(values)
+      }}
+      onFinishFailed={(error) => {
+        console.error(error)
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Tên danh mục"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: 'Vui lòng nhập tên danh mục!',
+          },
+        ]}
+        style={{fontWeight: 600}}
+        hasFeedback
+      >
+        <Input placeholder='Tên danh mục mới'/>
+      </Form.Item>
+
+      <Form.Item
+        label="Mô tả danh mục mới"
+        name="description"
+        style={{fontWeight: 600}}
+
+      >
+        <Input placeholder='Mô tả danh mục mới'/>
+      </Form.Item>
+    </Form>
+      </Modal>
     </Content>
     </Layout>
   )
