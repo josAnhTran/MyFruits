fd5420264c61911d31710b2b313a883e55ecb3cc
josAnhTran <jostranvananh@gmail.com>
2022-10-28 10:05:15 +0700

    updated images

---
+++ BE-onlineShop/express-app/functions/constants.js
@@ -0,0 +1,14 @@
+const URL_DATA_SERVER = "mongodb://127.0.0.1:27017/";
+const URL_APP_SERVER = 'mongodb://127.0.0.1:27017/online-shop';
+const PATH_FOLDER_PUBLIC = "./public";
+const PATH_FOLDER_IMAGES= '/images' ;
+const FOLDER_INITIATION = 'initiation';
+
+
+module.exports= {
+    URL_DATA_SERVER,
+    URL_APP_SERVER,
+    PATH_FOLDER_PUBLIC,
+    PATH_FOLDER_IMAGES,
+    FOLDER_INITIATION,
+}
